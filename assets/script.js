723be2d812447f4c6d0b2275f61322a8992074ba
Sander Ruys <ruyssander@gmail.com>
2022-05-23 16:50:09 +0200

    Fix github issues

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,33 @@
 import { key } from './key.js';
+const weatherInfoCo = document.getElementById("weather-info-co")
 const lat = "50.99317423134494"; 
 const lon = "3.875852782814391";
 const weatherApiUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${key}`;
-fetch(weatherApiUrl)
+
+/*fetch(weatherApiUrl)
     .then(response => response.json())
     .then(data => console.log(data))
-    .then(err => console.log(err));
+    
+    .then(err => console.log(err));*/
+
+
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    
+    show(data);
+}
+
+// Calling that async function
+getapi(weatherApiUrl);
+  
+const show = (data) =>{
+    console.log(data.list[0].components.co);
+} 
+
