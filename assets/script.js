b8a2838aa3e10483e1b6062fd499a8a08d036fe2
Sander Ruys <ruyssander@gmail.com>
2022-05-27 12:21:09 +0200

    Add UV Index

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 import { Weather_API_key, PositionStack_API_Key } from './config.js';
-const cityName = document.getElementById("city-name");
+
 
 const weatherCard = document.querySelector(".weatherCard");
 const weatherInfo = document.getElementById("weather-info");
@@ -26,6 +26,7 @@ let currentWeatherIcon;
 let cityInput = document.getElementById("input-city");
 let foreCastIconSrc = "#";
 let foreCastIconAlt = "#";
+let cityName = "";
 
 
 
@@ -72,7 +73,8 @@ async function getPositionApi(url) {
     console.log("adres = " + outputAdress);
 
     //cityName.innerText = data.data[0].locality;
-    console.log("gemeente= " + data.data[0].locality);
+    cityName = data.data[0].locality;
+    console.log("gemeente= " + cityName);
 
     // call weather API
     const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={minutely}&units=metric&appid=${Weather_API_key}`;
@@ -88,7 +90,7 @@ async function getOneCallApi(url) {
 
     
     setWeatherData(data);
-    createWeather(data, foreCastIconSrc, foreCastIconAlt);
+    createWeather(data, foreCastIconSrc, foreCastIconAlt, cityName);
     console.log("WeatherData= " + foreCastIconSrc);
     
 }
@@ -120,26 +122,31 @@ const setWeatherData = (data) => {
     }
 }
 
-const createWeather = (data, iconSrc, iconAlt) =>{
+const createWeather = (data, iconSrc, iconAlt, cityName) =>{
     let code = `
-        <h1 id="city-name">Hallo</h1>
+        <h1 id="city-name">${cityName}</h1>
         <img src="${iconSrc}" alt="${iconAlt}" id="forecast-icon">
-        <p id="forecast-degrees"></p>
-        <p id="forecast-description"></p>
+        <p id="forecast-degrees">${data.current.temp} &#8451;</p>
+        <p id="forecast-description">${data.current.weather[0].description}</p>
         <div class="feelsLike">
-            <img src="${iconSrc}" alt="${iconAlt}" id="feels-like-icon">
+            <img src="${iconSrc}" alt="${iconAlt}" class="smallIcon">
             <p id="feels-like-text">Feels Like</p>
-            <p id="feels-like-degrees"></p>
+            <p id="feels-like-degrees">${data.current.feels_like} &#8451;</p>
         </div>
         <div class="humidity">
-            <img src="assets/Humidity.png" alt="Humidity icon" id="humidity-icon">
+            <img src="assets/Humidity.png" alt="Humidity icon" class="smallIcon"">
             <p id="humidity-text">Humidity</p>
-            <p id="humidity-degrees"></p>
+            <p id="humidity-percent">${data.current.humidity} &#37;</p>
         </div>
         <div class="wind">
-            <img src="assets/Wind.png" alt="Wind icon" id="humidity-icon">
-            <p id="humidity-text">Wind</p>
-            <p id="humidity-degrees"></p>
+            <img src="assets/Wind.png" alt="Wind icon" class="smallIcon"">
+            <p id="wind-text">Wind</p>
+            <p id="wind-speed">${data.current.wind_speed} km&#47;h</p>
+        </div>
+        <div class="uvIndex">
+            <img src="assets/Sun.png" alt="Sun icon" class="smallIcon"">
+            <p id="uv-text">UV&#45;Index</p>
+            <p id="uv-index">${data.current.uvi}</p>
         </div>
         `;
         weatherCard.innerHTML += code;
